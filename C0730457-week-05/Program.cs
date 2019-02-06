6ab1452d2730fd324c188af7c13da61edee6a79e
visualstudio <visualstudio@122-PC13>
2019-02-06 10:08:53 -0500

    commi

--- C0730457-week-05/Program.cs
+++ C0730457-week-05/Program.cs
@@ -4,9 +4,36 @@ namespace C0730457_week_05
 {
     class Program
     {
+        static int i = 0;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            {
+                MethodA();
+            }
+
+             static void MethodA()
+
+            {
+                while (Peanut())
+                {
+                    Console.WriteLine("i is {0}" , i)
+                        if (i > 10)
+                    {
+                        return; ;
+                }
+
+            }
+
+             static bool Peanut()
+
+            {
+                i++;
+                return true;
+            }
+        }
+       
+
         }
     }
-}
+
